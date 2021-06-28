01d590a8ca175b35d0a49d45d0c962cdef53f131
whitekite <1075790909@qq.com>
2021-06-28 10:07:35 +0800

    修改身份证工具导出逻辑

--- src/extra/index.ts
+++ src/extra/index.ts
@@ -3,7 +3,7 @@
  * @author WhiteKite
  */
 export * from './StrUtil'
-export { default as IdCardUtil } from './IdCardUtil'
+export { default as IdCardUtil, IdCard } from './IdCardUtil'
 export * from './MobileUtil'
 export * from './BooleanUtil'
 export { default as Base64Util } from './Base64Util'
