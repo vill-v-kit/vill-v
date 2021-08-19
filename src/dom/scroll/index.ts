cb5cd8c3ca311f9c611370381ddd61df01b005f5
whitekite <1075790909@qq.com>
2021-08-19 15:38:28 +0800

    [修复] raf 工具未正确导出的问题 （fork ant-design-vue） [新增] 样式是否支持检测 （fork ant-design-vue）

--- src/dom/scroll/index.ts
+++ src/dom/scroll/index.ts
@@ -1,2 +1,3 @@
 export { default as scrollTo } from './scrollTo'
 export { default as getScroll, isWindow } from './getScroll'
+export { default as raf } from './raf'
--- src/dom/style/styleChecker.ts
+++ src/dom/style/styleChecker.ts
@@ -1,15 +1,11 @@
 /**
  * @licence https://github.com/vueComponent/ant-design-vue
  */
-function canUseDom() {
-  return !!(
-    typeof window !== 'undefined' &&
-    window.document &&
-    window.document.createElement
-  )
+export function canUseDom() {
+  return !!(typeof window !== 'undefined' && window.document && window.document.createElement)
 }
 
-const canUseDocElement = () => canUseDom() && window.document.documentElement
+export const canUseDocElement = () => canUseDom() && window.document.documentElement
 
 const isStyleSupport = (styleName: string | Array<string>): boolean => {
   if (canUseDocElement()) {
@@ -19,34 +15,6 @@ const isStyleSupport = (styleName: string | Array<string>): boolean => {
     return styleNameList.some((name) => name in documentElement.style)
   }
   return false
-}
-
-let flexGapSupported: boolean | undefined
-export const detectFlexGapSupported = () => {
-  if (!canUseDocElement()) {
-    return false
-  }
-
-  if (flexGapSupported !== undefined) {
-    return flexGapSupported
-  }
-
-  // create flex container with row-gap set
-  const flex = document.createElement('div')
-  flex.style.display = 'flex'
-  flex.style.flexDirection = 'column'
-  flex.style.rowGap = '1px'
-
-  // create two, elements inside it
-  flex.appendChild(document.createElement('div'))
-  flex.appendChild(document.createElement('div'))
-
-  // append to the DOM (needed to obtain scrollHeight)
-  document.body.appendChild(flex)
-  flexGapSupported = flex.scrollHeight === 1 // flex container should be 1px high from the row-gap
-  document.body.removeChild(flex)
-
-  return flexGapSupported
 }
 
 export default isStyleSupport
