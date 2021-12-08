d51f1c535d8cb98fa7cbf15964f46c44270bd1e4
xuxinjie <1075790909@qq.com>
2021-12-08 15:29:08 +0800

    [新增] 一个用于处理💩山时，项目过多log的突发奇想的输出工具类，试验性质，不保证可用

---
+++ src/log.ts
@@ -0,0 +1,84 @@
+/**
+ * 一个用于处理💩山时，项目过多log的突发奇想
+ * 试验性质，不保证可用
+ */
+export class GroupLog {
+  public prod = false
+
+  constructor(name: string, prod?: boolean) {
+    this.clear()
+    this.create(name)
+    this.setProd(prod)
+  }
+
+  public setProd(prod?: boolean) {
+    this.prod = prod || false
+    return this
+  }
+
+  public get console() {
+    if (!this.prod) {
+      return console
+    }
+    return null
+  }
+
+  public clear() {
+    this.console?.clear()
+    return this
+  }
+
+  private create(name: string) {
+    this.console?.group(name)
+    return this
+  }
+
+  public dispose() {
+    this.console?.groupEnd()
+  }
+
+  public log(...args: any[]) {
+    this.console?.log(args)
+    return this
+  }
+
+  public warn(...args: any[]) {
+    this.console?.warn(args)
+    return this
+  }
+
+  public error(...args: any[]) {
+    this.console?.error(args)
+    return this
+  }
+
+  public count(label?: string) {
+    this.console?.count(label)
+    return this
+  }
+
+  public countReset(label?: string) {
+    this.console?.countReset(label)
+    return this
+  }
+
+  public info(...args: any[]) {
+    this.console?.info(args)
+    return this
+  }
+
+  public table(tabularData?: any, properties?: string[]) {
+    this.console?.table(tabularData, properties)
+    return this
+  }
+
+  public time(label?: string) {
+    this.console?.time(label)
+    return this
+  }
+
+  public timeEnd(label?: string) {
+    this.console?.timeEnd(label)
+    return this
+  }
+}
