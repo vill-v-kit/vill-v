f26d9e35c287618c2e67f6f3e0feab2554ef9fcd
xuxinjie <1075790909@qq.com>
2022-02-21 10:53:42 +0800

    [修改] StorageCacheStore 的格式parse方式修改为用户自定义

--- src/cache/storage.ts
+++ src/cache/storage.ts
@@ -1,5 +1,4 @@
-import { forceJsonParse } from '../json'
-import { isNil, isString } from 'lodash-es'
+import { isNil } from 'lodash-es'
 
 export type StorageCacheStoreType = typeof localStorage | typeof sessionStorage
 
@@ -13,17 +12,26 @@ const StorageCacheStoreEnumObj = {
   [StorageCacheStoreEnum.Session]: sessionStorage,
 }
 
+export interface StorageCacheStoreParseType<T = any> {
+  get: (value: string) => T
+  set: (value: T) => string
+}
+
+const NormalStorageCacheStoreParse = {
+  get: (value) => value,
+  set: (value) => value,
+}
 /**
  * Storage缓存商店
  */
 export class StorageCacheStore<T = any> {
   private readonly storage: StorageCacheStoreType
-  private readonly json: boolean
+  private readonly parse: StorageCacheStoreParseType<T>
   private readonly key: string
 
-  constructor(storage: StorageCacheStoreEnum, key: string, json?: boolean) {
+  constructor(storage: StorageCacheStoreEnum, key: string, parse?: StorageCacheStoreParseType<T>) {
     this.storage = StorageCacheStoreEnumObj[storage]
-    this.json = json || false
+    this.parse = parse || NormalStorageCacheStoreParse
     this.key = key
   }
 
@@ -32,21 +40,16 @@ export class StorageCacheStore<T = any> {
     if (!value) {
       return null
     }
-    if (this.json) {
-      return forceJsonParse<T>(value, 'null') as any as T
-    }
-    return value as any as T
+    return this.parse.get(value)
   }
 
   set(value?: T | null) {
     if (isNil(value)) {
       return
     }
-    if (this.json) {
-      this.storage.setItem(this.key, isString(value) ? value : (JSON.stringify(value) as any))
-      return
-    }
-    this.storage.setItem(this.key, value as any)
+    const setValue = this.parse.set(value)
+
+    this.storage.setItem(this.key, setValue as any)
   }
 
   get value(): T | null {
