4d739095b30296a56f5134d67747338f93635ed8
whitekite <1075790909@qq.com>
2022-05-31 15:32:08 +0800

    修改：项目依赖升级

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
-// @ts-check
-const { defineConfig } = require('eslint-define-config')
 require('@rushstack/eslint-patch/modern-module-resolution')
-module.exports = defineConfig({
+module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2020,
@@ -11,4 +9,4 @@ module.exports = defineConfig({
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
-})
+}
