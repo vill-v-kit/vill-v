5c0c0687cede7c710267e4dbc01ed5c0e60cad2b
whitekite <1075790909@qq.com>
2022-07-06 12:14:42 +0800

    feat(path-prop):防止原型链污染

--- packages/path-prop/src/pathIterator.ts
+++ packages/path-prop/src/pathIterator.ts
@@ -2,6 +2,7 @@ import { Path } from './interface'
 import { isNil, isObject } from '@vill-v/type-as'
 import { pathParse } from './pathParse'
 
+const unsafePath = new Set(['__proto__', 'prototype', 'constructor'])
 export const pathIterator = <T = any>(
   value: any,
   path: Path,
@@ -14,6 +15,9 @@ export const pathIterator = <T = any>(
     return callback(true, value)
   }
   const pathArr = pathParse(path)
+  if (pathArr.findIndex((item) => unsafePath.has(`${item}`)) !== -1) {
+    return undefined as any as T
+  }
   let res = callback(true, value)
   pathArr.every((pathItem) => {
     if (!isObject(value) || !(pathItem in value)) {
