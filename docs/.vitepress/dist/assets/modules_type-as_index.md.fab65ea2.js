5a11e25f084fb20f4f094306caddf2fb988689f1
whitekite <1075790909@qq.com>
2022-11-08 01:00:34 +0800

    docs: build docs

---
+++ docs/.vitepress/dist/assets/modules_type-as_index.md.fab65ea2.js
@@ -0,0 +1,29 @@
+import{_ as s,c as n,o as a,a as l}from"./app.90705f79.js";const F=JSON.parse('{"title":"\u4ECB\u7ECD","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5B89\u88C5","slug":"\u5B89\u88C5","link":"#\u5B89\u88C5","children":[]},{"level":2,"title":"\u53EF\u5224\u65AD\u7684\u7C7B\u578B","slug":"\u53EF\u5224\u65AD\u7684\u7C7B\u578B","link":"#\u53EF\u5224\u65AD\u7684\u7C7B\u578B","children":[]}],"relativePath":"modules/type-as/index.md","lastUpdated":1655051395000}'),p={name:"modules/type-as/index.md"},e=l(`<h1 id="\u4ECB\u7ECD" tabindex="-1">\u4ECB\u7ECD <a class="header-anchor" href="#\u4ECB\u7ECD" aria-hidden="true">#</a></h1><p>\u5F00\u53D1\u4E2D\u603B\u7ED3\u7684\u7C7B\u578B\u5224\u65AD</p><h2 id="\u5B89\u88C5" tabindex="-1">\u5B89\u88C5 <a class="header-anchor" href="#\u5B89\u88C5" aria-hidden="true">#</a></h2><p>\u4F7F\u7528npm</p><div class="language-bash"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">$ npm i @vill-v/type-as</span></span>
+<span class="line"></span></code></pre></div><p>\u4F7F\u7528yarn</p><div class="language-bash"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">$ yarn add @vill-v/type-as</span></span>
+<span class="line"></span></code></pre></div><p>\u4F7F\u7528pnpm</p><div class="language-bash"><button title="Copy Code" class="copy"></button><span class="lang">bash</span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">$ pnpm add @vill-v/type-as</span></span>
+<span class="line"></span></code></pre></div><h2 id="\u53EF\u5224\u65AD\u7684\u7C7B\u578B" tabindex="-1">\u53EF\u5224\u65AD\u7684\u7C7B\u578B <a class="header-anchor" href="#\u53EF\u5224\u65AD\u7684\u7C7B\u578B" aria-hidden="true">#</a></h2><div class="language-ts"><button title="Copy Code" class="copy"></button><span class="lang">ts</span><pre class="shiki"><code><span class="line"><span style="color:#82AAFF;">isBoolean</span><span style="color:#A6ACCD;">(</span><span style="color:#FF9CAC;">true</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isError</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">new</span><span style="color:#A6ACCD;"> </span><span style="color:#82AAFF;">Error</span><span style="color:#A6ACCD;">())</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isFunction</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">()</span><span style="color:#A6ACCD;"> </span><span style="color:#C792EA;">=&gt;</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">null</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isMap</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">new</span><span style="color:#A6ACCD;"> </span><span style="color:#82AAFF;">Map</span><span style="color:#A6ACCD;">())</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isNil</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">null</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isNil</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">undefined</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isNull</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">null</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isNumber</span><span style="color:#A6ACCD;">(</span><span style="color:#F78C6C;">1</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isObject</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">{</span><span style="color:#F07178;">foor</span><span style="color:#89DDFF;">:</span><span style="color:#A6ACCD;"> </span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">bar</span><span style="color:#89DDFF;">&#39;</span><span style="color:#89DDFF;">}</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isSet</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">new</span><span style="color:#A6ACCD;"> </span><span style="color:#82AAFF;">Set</span><span style="color:#A6ACCD;">())</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isString</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">&#39;</span><span style="color:#C3E88D;">foor</span><span style="color:#89DDFF;">&#39;</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isSymbol</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">new</span><span style="color:#A6ACCD;"> </span><span style="color:#82AAFF;">Symbol</span><span style="color:#A6ACCD;">())</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#82AAFF;">isUndefined</span><span style="color:#A6ACCD;">(</span><span style="color:#89DDFF;">undefined</span><span style="color:#A6ACCD;">)</span></span>
+<span class="line"></span></code></pre></div>`,11),o=[e];function c(t,r,A,i,y,C){return a(),n("div",null,o)}const d=s(p,[["render",c]]);export{F as __pageData,d as default};
