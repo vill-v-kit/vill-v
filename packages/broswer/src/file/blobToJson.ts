d06e13efed4a9631de7141171580b2eaf9b1d25d
whitekite <1075790909@qq.com>
2023-07-11 02:49:25 +0800

    feat(broswer): `blobToJson` 可以根据兼容性自适应使用更现代的  `blob.text()` 特性 ，并开放 `blobToJsonModern` `blobToJsonLegacy`  函数支持直接分别调用新老 特性

--- packages/broswer/src/file/blobToJson.ts
+++ packages/broswer/src/file/blobToJson.ts
@@ -4,10 +4,15 @@ import { isString } from '@vill-v/type-as'
 /**
  * blob转json
  * 用于axios选择转换为blob时，对后台正常json报错进行读取
+ *
+ * 使用 `FileReader` 进行转换
  * @param blob
  * @param format
  */
-export function blobToJson(blob: Blob, format = 'utf-8'): Promise<Record<string, any> | null> {
+export const blobToJsonLegacy = <T extends Record<string, any> = Record<string, any>>(
+  blob: Blob,
+  format = 'utf-8'
+): Promise<T | null> => {
   return new Promise((resolve, reject) => {
     const fileReader = new FileReader()
     fileReader.onload = (res) => {
@@ -30,4 +35,37 @@ export function blobToJson(blob: Blob, format = 'utf-8'): Promise<Record<string,
     }
     fileReader.readAsText(blob, format)
   })
+}
+
+/**
+ * blob转json
+ * 用于axios选择转换为blob时，对后台正常json报错进行读取
+ *
+ * 使用 `blob.text()` 进行转换
+ * @param blob
+ */
+export const blobToJsonModern = async <T extends Record<string, any> = Record<string, any>>(
+  blob: Blob
+): Promise<T> => {
+  const result = await blob.text()
+  return JSON.parse(result)
+}
+
+/**
+ * blob转json
+ * 用于axios选择转换为blob时，对后台正常json报错进行读取
+ *
+ *  在现代浏览器 使用 `blob.text()` 进行转换
+ *  在不支持该特性的浏览器内 使用 `FileReader` 进行转换
+ * @param blob
+ * @param format
+ */
+export function blobToJson<T extends Record<string, any> = Record<string, any>>(
+  blob: Blob,
+  format = 'utf-8'
+): Promise<T | null> {
+  if (Reflect.has(Blob.prototype, 'text')) {
+    return blobToJsonModern<T>(blob)
+  }
+  return blobToJsonLegacy<T>(blob, format)
 }
