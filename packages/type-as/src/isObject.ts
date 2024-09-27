e98aa651097004a87a7579824d38b110faadd392
whitekite <1075790909@qq.com>
2024-09-27 09:53:03 +0800

    fix(type-as): 修复 `isObject` 结果不符合预期的问题

--- packages/type-as/src/isObject.ts
+++ packages/type-as/src/isObject.ts
@@ -1,4 +1,2 @@
-import { typeOf } from './typeOf'
-
 export const isObject = <Obj extends Record<string, any>>(value: any): value is Obj =>
-  !!value && typeOf(value) === 'object'
+  !!value && value.constructor === Object
